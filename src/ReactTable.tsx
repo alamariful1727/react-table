65863f0eba0b1afd95ad25ea18a16f90299498f4
Ariful Alam <alamariful1727@gmail.com>
2020-09-03 23:34:22 +0600

    Pagination added with a very basic UI implementation

--- src/ReactTable.tsx
+++ src/ReactTable.tsx
@@ -1,21 +1,53 @@
 import React, { ReactElement, PropsWithChildren } from 'react';
-import { useTable, TableOptions, useSortBy } from 'react-table';
+import { useTable, TableOptions, useSortBy, usePagination } from 'react-table';
 import { Table, TableHead, TableRow, TableCell, TableBody, TableContainer, TableSortLabel } from '@material-ui/core';
 
 interface ReactTableProps<T extends object = {}> extends TableOptions<T> {}
 
 export function ReactTable<T extends object>(props: PropsWithChildren<ReactTableProps<T>>): ReactElement {
   const { data, columns } = props;
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    pageCount,
+    canNextPage,
+    canPreviousPage,
+    state: { pageIndex, pageSize },
+    gotoPage,
+    nextPage,
+    previousPage,
+    pageOptions,
+    setPageSize,
+  } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 0, pageSize: 3 },
     },
     useSortBy,
+    usePagination,
   );
 
   return (
     <div>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage,
+            },
+            null,
+            2,
+          )}
+        </code>
+      </pre>
       <TableContainer>
         <Table {...getTableProps()}>
           <TableHead>
@@ -34,7 +66,7 @@ export function ReactTable<T extends object>(props: PropsWithChildren<ReactTable
             ))}
           </TableHead>
           <TableBody {...getTableBodyProps()}>
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row);
               return (
                 <TableRow {...row.getRowProps()}>
@@ -47,6 +79,50 @@ export function ReactTable<T extends object>(props: PropsWithChildren<ReactTable
           </TableBody>
         </Table>
       </TableContainer>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[3, 5, 10].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
